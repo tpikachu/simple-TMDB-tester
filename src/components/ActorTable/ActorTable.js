80b95df447fd22d8c716273fde654cff2d916d3b
SJhumili <sjkunai@outlook.com>
2019-01-18 03:03:31 +0200

    ActorTable

--- src/components/ActorTable/ActorTable.js
+++ src/components/ActorTable/ActorTable.js
@@ -1,14 +1,28 @@
 import React from "react";
 import {connect} from 'react-redux';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {Grid, Row, Thumbnail, Button} from 'react-bootstrap';
 //https://image.tmdb.org/t/p/original/lhKObAfoVDU6as0FCgm6y2AAeCO.jpg
 
 class ActorTable extends React.Component {
     render() {
         console.log(this.props.imoviecasts);
-      return(
-          <h1>I'm ActorTable</h1>
-      );
+        const imoviecasts = this.props.imoviecasts;
+        return(
+            <Grid>
+            {
+                imoviecasts.map(cast => {
+                    return (
+                        <Row key={cast.credit_id} style={{width:'20%'}}>
+                            <Thumbnail key={cast.credit_id} src={`https://image.tmdb.org/t/p/original/${cast.profile_path}`} alt="242x200">
+                                <h3>{cast.character}</h3>
+                                <p>{cast.name}</p>
+                            </Thumbnail>
+                        </Row>
+                    );
+                })
+            }
+            </Grid>
+        );
     }
 }
 const mapStatetoProps  = ({movietable}) =>{
